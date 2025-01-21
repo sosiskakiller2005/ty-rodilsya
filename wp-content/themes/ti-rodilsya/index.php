84e13d644b98689824d824fc4bc97fcf8938e0f3
Vladislav Otdelyanov <vlad_1995@mail.ru>
2025-01-21 22:58:20 +0300

    Добавлен динамический header и загрузка стилей и скриптов.

--- wp-content/themes/ti-rodilsya/index.php
+++ wp-content/themes/ti-rodilsya/index.php
@@ -1,39 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ты родился!</title>
-
-    <link rel="stylesheet" href="./assets/css/main.css">
-</head>
-
-<body>
-    <div class="container">
-        <header class="header">
-            <div class="header__left">
-                <a href="./index.html">
-                    <img src="./assets/img/logo.png" alt="Логотип" class="header__logo">
-                </a>
-            </div>
-            <div class="header__right">
-                <div class="header__social">
-                </div>
-                <nav class="header__nav">
-                    <a href="#" class="header__nav-item">Видеооткрытка с фильмами 1934 - 1994 год</a>
-                    <a href="#" class="header__nav-item">Видеооткрытка с фильмами к праздникам</a>
-                    <a href="#" class="header__nav-item">Индивидуальный проект</a>
-                    <a href="#" class="header__nav-item">Оптовым покупателям</a>
-                    <a href="#" class="header__nav-item">Корпоративным заказчикам</a>
-                </nav>
-                <div class="header__login">
-                    <a href="#" class="header_login-link">У меня есть код. Смотреть онлайн</a>
-                    <a href="./page-registration.html" class="header_login-link">Регистрация покупателя</a>
-                    <a href="./page-login.html" class="header_login-link">Вход</a>
-                </div>
-            </div>
-        </header>
+<?php get_header() ?>
 
         <main class="main-content">
             <article class="article-container">
@@ -135,8 +100,7 @@
 
         </div>
     </footer>
-
-    <script src="./assets/js/script.js"></script>
+    <?php wp_footer(); ?>
 </body>
 
 </html>
