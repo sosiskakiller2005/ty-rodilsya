7a48caa669d6b73110c2bf9f13f2794b5797df63
Влад Козлов <vkozlov15012005@gmail.com>
2025-01-21 23:11:59 +0300

    Создан динамический футер

---
+++ wp-content/themes/ti-rodilsya/footer.php
@@ -0,0 +1,22 @@
+<footer class="footer">
+        <div class="footer-item">
+            <a class="footer-item-link" href="#">- ФИЛЬМЫ ПО ГОДАМ</a>
+            <a class="footer-item-link" href="#">- ФИЛЬМЫ К ПРАЗДНИКАМ</a>
+            <a class="footer-item-link" href="#">- ИНДИВИДУАЛЬНОЕ ПРЕДЛОЖЕНИЕ</a>
+        </div>
+        <div class="footer-item">
+            <a class="footer-item-link" href="#">- ОПТОВЫМ ПОКУПАТЕЛЯМ</a>
+            <a class="footer-item-link" href="#">- КОРПОРАТИВНЫМ ЗАКАЗЧИКАМ</a>
+            <a class="footer-item-link" href="#">- УНИВЕРМАН ПОДАРКОВ</a>
+        </div>
+        <div class="footer-copyright">
+            <div>
+
+            </div>
+
+        </div>
+    </footer>
+    <?php wp_footer(); ?>
+</body>
+
+</html>
--- wp-content/themes/ti-rodilsya/index.php
+++ wp-content/themes/ti-rodilsya/index.php
@@ -82,25 +82,4 @@
         </main>
     </div>
 
-    <footer class="footer">
-        <div class="footer-item">
-            <a class="footer-item-link" href="#">- ФИЛЬМЫ ПО ГОДАМ</a>
-            <a class="footer-item-link" href="#">- ФИЛЬМЫ К ПРАЗДНИКАМ</a>
-            <a class="footer-item-link" href="#">- ИНДИВИДУАЛЬНОЕ ПРЕДЛОЖЕНИЕ</a>
-        </div>
-        <div class="footer-item">
-            <a class="footer-item-link" href="#">- ОПТОВЫМ ПОКУПАТЕЛЯМ</a>
-            <a class="footer-item-link" href="#">- КОРПОРАТИВНЫМ ЗАКАЗЧИКАМ</a>
-            <a class="footer-item-link" href="#">- УНИВЕРМАН ПОДАРКОВ</a>
-        </div>
-        <div class="footer-copyright">
-            <div>
-
-            </div>
-
-        </div>
-    </footer>
-    <?php wp_footer(); ?>
-</body>
-
-</html>
+    <?php get_footer() ?>
