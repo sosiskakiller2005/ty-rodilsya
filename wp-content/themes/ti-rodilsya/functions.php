7b71a09d1fbb11beab7a12f784decefa1114035f
Vladislav Otdelyanov <vlad_1995@mail.ru>
2025-01-22 20:51:46 +0300

    Добавил стиль для медиа-запросов.

--- wp-content/themes/ti-rodilsya/functions.php
+++ wp-content/themes/ti-rodilsya/functions.php
@@ -5,6 +5,7 @@ add_action( 'wp_enqueue_scripts', 'upload_scripts' );
 function upload_scripts() {
 	wp_enqueue_style( 'main', get_stylesheet_uri() );
 	wp_enqueue_style( 'fontawesome', get_template_directory_uri() . '/assets/css/all.css' );
+	wp_enqueue_style( 'mediaquery', get_template_directory_uri() . '/assets/css/media.css' );
 	wp_enqueue_script( 'script', get_template_directory_uri() . '/assets/js/script.js');
 }
 ?>
