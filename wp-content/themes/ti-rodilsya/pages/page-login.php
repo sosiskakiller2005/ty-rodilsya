c77334a2ef14660f89a4b064a3150ded1dab5868
Vladislav Otdelyanov <vlad_1995@mail.ru>
2025-01-22 19:53:43 +0300

    Добавлен стиль для иконочных шрифтов.

--- wp-content/themes/ti-rodilsya/pages/page-login.php
+++ wp-content/themes/ti-rodilsya/pages/page-login.php
@@ -5,24 +5,23 @@ Template Post Type: page
 */
 ?>
 <section class="section-login">
-            <form class="login-form">
-                <h2>Вход</h2>
-                <p>Авторизуйтесь через социальные сети</p>
-                <div class="login-socials">
-                    <a class="social-link" href="#">
-                        <i class="fab fa-vk"></i>
-                    </a>
-                    <a class="social-link" href="#">
-                        <i class="fab fa-odnoklassniki"></i>
-                    </a>
-                </div>
-    
-                <p>или используйте свой аккаут</p>
-                <input type="text" name="" id="" placeholder="Введите почту">
-                <input type="password" name="" id="" placeholder="Введите пароль">
-                <a href="#">Забыли пароль?</a>
-                <button>Войти</button>
-            </form>
-        </section>
-    </div>
+    <form class="login-form">
+        <h2>Вход</h2>
+        <p>Авторизуйтесь через социальные сети</p>
+        <div class="login-socials">
+            <a class="social-link" href="#">
+                <i class="fa-brands fa-odnoklassniki-square"></i>
+            </a>
+            <a class="social-link" href="#">
+                <i class="fa-brands fa-vk"></i>
+            </a>
+        </div>
 
+        <p>или используйте свой аккаут</p>
+        <input type="text" name="" id="" placeholder="Введите почту">
+        <input type="password" name="" id="" placeholder="Введите пароль">
+        <a href="#">Забыли пароль?</a>
+        <button>Войти</button>
+    </form>
+</section>
+</div>
--- wp-content/themes/ti-rodilsya/pages/page-registration.php
+++ wp-content/themes/ti-rodilsya/pages/page-registration.php
@@ -8,33 +8,33 @@ Template Post Type: page
 
 
 <section class="section-login">
-                <form class="login-form">
-                    <h2>Регистрация</h2>
-                    <p>Авторизуйтесь через социальные сети</p>
-                    <div class="login-socials">
-                        <a class="social-link" href="#">
-                            <i class="fab fa-vk"></i>
-                        </a>
-                        <a class="social-link" href="#">
-                            <i class="fab fa-odnoklassniki"></i>
-                        </a>
-                    </div>
-        
-                    <p>или используйте свой аккаут</p>
-                    <input type="text" name="" id="" placeholder="Введите имя">
-                    <div class="input-hint">
-                        <input type="text" name="" id="" placeholder="Введите почту">
-                        <p class="hint">На этот адрес будут отправляться уведомления о статусе заказа</p>
-                    </div>
-                    <div class="input-hint">
-                        <input type="password" name="" id="" placeholder="Введите пароль">
-                        <p class="hint">Назначьте пароль или мы сгенерируем его автоматически</p>
-                    </div>
-                    
-                    <div class="captcha">
-
-                    </div>
-                    <button>Зарегистрироваться</button>
-                </form>
-            </section>
+    <form class="login-form">
+        <h2>Регистрация</h2>
+        <p>Авторизуйтесь через социальные сети</p>
+        <div class="login-socials">
+            <a class="social-link" href="#">
+                <i class="fa-brands fa-odnoklassniki-square"></i>
+            </a>
+            <a class="social-link" href="#">
+                <i class="fa-brands fa-vk"></i>
+            </a>
         </div>
+
+        <p>или используйте свой аккаут</p>
+        <input type="text" name="" id="" placeholder="Введите имя">
+        <div class="input-hint">
+            <input type="text" name="" id="" placeholder="Введите почту">
+            <p class="hint">На этот адрес будут отправляться уведомления о статусе заказа</p>
+        </div>
+        <div class="input-hint">
+            <input type="password" name="" id="" placeholder="Введите пароль">
+            <p class="hint">Назначьте пароль или мы сгенерируем его автоматически</p>
+        </div>
+
+        <div class="captcha">
+
+        </div>
+        <button>Зарегистрироваться</button>
+    </form>
+</section>
+</div>
