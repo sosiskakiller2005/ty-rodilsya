bd57cd26b613e37bba788cff30530d174f7978c3
Влад Козлов <vkozlov15012005@gmail.com>
2025-01-22 22:32:45 +0300

    Добавлен скрипт работы бургер-меню

--- wp-content/themes/ti-rodilsya/assets/js/script.js
+++ wp-content/themes/ti-rodilsya/assets/js/script.js
@@ -14,3 +14,26 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const burgerMenu = document.querySelector(".header__hamburger-btn");
+  const menu = document.querySelector(".menu");
+  const closeBtn = document.getElementById("closeBtn");
+
+  burgerMenu.addEventListener("click", () => {
+    menu.classList.toggle("active");
+
+    // Отключаем прокрутку страницы, когда меню открыто
+    if (menu.classList.contains("active")) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+  });
+
+  closeBtn.addEventListener("click", () => {
+    menu.classList.remove("active");
+  })
+});
+
+document.addEventListener()
