e0339bf86f5bc5b94b67e6cbb6d825aa1a9efa4b
Vladislav Otdelyanov <vlad_1995@mail.ru>
2025-01-23 21:42:22 +0300

    Добавлен дизайн страницы одиночного продукта.

---
+++ wp-content/themes/ti-rodilsya/pages/product.php
@@ -0,0 +1,15 @@
+<section class="single-product">
+    <h3>Название продукта</h3>
+    <div class="single-product-content">
+        <img src="" alt="">
+        <p>Описание</p>
+    </div>
+    <div class="single-product-button">
+        <p>Цена</p>
+        <a href="#" class="product-button">
+            <i class="fa-solid fa-cart-shopping"></i>
+            <span>В корзину</span>
+        </a>
+    </div>
+</section>
+</div>
