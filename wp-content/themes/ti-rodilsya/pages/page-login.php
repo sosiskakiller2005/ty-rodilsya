9f663c0ad1c941cf38e60537c29c3719f6a41f6f
Влад Козлов <vkozlov15012005@gmail.com>
2025-01-24 21:53:56 +0300

    Добавлена авторизация через базу данных, вконтакте

--- wp-content/themes/ti-rodilsya/pages/page-login.php
+++ wp-content/themes/ti-rodilsya/pages/page-login.php
@@ -3,52 +3,26 @@
 Template Name: Шаблон страницы входа
 */
 ?>
+
+<?php
+if (is_user_logged_in()) {
+  wp_redirect(home_url()); // Перенаправление на главную, если пользователь уже авторизован
+  exit;
+}
+?>
+<?php get_header(); ?>
+
 <section class="section-login">
-    <form class="login-form">
+  <?php
+      if (isset($_GET['login']) && $_GET['login'] == 'failed') {
+          echo '<div class="error-message">Ошибка: Неправильный логин или пароль.</div>';
+      }
+      ?>
+    <form class="login-form" method="post" action="<?php echo esc_url(site_url('wp-login.php', 'login_post')); ?>">
         <h2>Вход</h2>
         <p>Авторизуйтесь через социальные сети</p>
         <div class="login-socials">
             <?php echo do_shortcode('[miniorange_social_login shape="square" theme="default" space="4" size="35"]'); ?>
-            <div>
-  <script src="https://unpkg.com/@vkid/sdk@<3.0.0/dist-sdk/umd/index.js"></script>
-  <script type="text/javascript">
-    if ('VKIDSDK' in window) {
-      const VKID = window.VKIDSDK;
-
-      VKID.Config.init({
-        app: 52957066,
-        redirectUrl: 'https://tyrodilsyatest.ru/openidcallback/vkontakte',
-        responseMode: VKID.ConfigResponseMode.Callback,
-        source: VKID.ConfigSource.LOWCODE,
-        scope: '', // Заполните нужными доступами по необходимости
-      });
-
-      const oneTap = new VKID.OneTap();
-
-      oneTap.render({
-        container: document.currentScript.parentElement,
-        showAlternativeLogin: true
-      })
-      .on(VKID.WidgetEvents.ERROR, vkidOnError)
-      .on(VKID.OneTapInternalEvents.LOGIN_SUCCESS, function (payload) {
-        const code = payload.code;
-        const deviceId = payload.device_id;
-
-        VKID.Auth.exchangeCode(code, deviceId)
-          .then(vkidOnSuccess)
-          .catch(vkidOnError);
-      });
-    
-      function vkidOnSuccess(data) {
-        // Обработка полученного результата
-      }
-    
-      function vkidOnError(error) {
-        // Обработка ошибки
-      }
-    }
-  </script>
-</div>
             <a class="social-link" href="#">
                 <i class="fa-brands fa-odnoklassniki-square"></i>
             </a>
@@ -58,13 +32,16 @@ Template Name: Шаблон страницы входа
         </div>
 
         <p>или используйте свой аккаут</p>
-        <input type="text" name="" id="" placeholder="Введите почту">
-        <input type="password" name="" id="" placeholder="Введите пароль">
+        <input type="text" name="log" id="username" placeholder="Введите почту" required>
+        <input type="password" name="pwd" id="password" placeholder="Введите пароль" required>
         <a href="#">Забыли пароль?</a>
-        <button>Войти</button>
+        <button name="wp-submit" id="wp-submit">Войти</button>
+        <input type="hidden" name="redirect_to" value="<?php echo esc_url(home_url()); ?>">
     </form>
 
 </div>
 
 </section>
 </div>
+
+<?php get_footer(); ?>
