86cc0422516399cf30a094f1565b0f2717ff14bf
Vladislav Otdelyanov <vlad_1995@mail.ru>
2025-01-25 20:43:39 +0300

    Добавлена секция профиля.

--- wp-content/themes/ti-rodilsya/pages/page-my-profile.php
+++ wp-content/themes/ti-rodilsya/pages/page-my-profile.php
@@ -4,6 +4,19 @@ Template Name: Шаблон страницы профиля
 */
 ?>
 <?php get_header(); ?>
-<h1>Страница профиля</h1>
+
+<section class="profile-section">
+    <nav class="profile-nav">
+        <a class="profile-nav-link" href="#">Мои заказы</a>
+        <span>></span>
+        <a class="profile-nav-link" href="#">Мои электронные открытки</a>
+        <span>></span>
+        <a class="profile-nav-link" href="#">Редактировать профиль</a>
+    </nav>
+    <div class="profile-content">
+
+    </div>
+</section>
+
 </div>
 <?php get_footer(); ?>
