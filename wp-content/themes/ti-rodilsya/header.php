08716c04be7ea3985df4506290f3d8de4b9500f9
Влад Козлов <vkozlov15012005@gmail.com>
2025-01-25 13:48:01 +0300

    Добавлены работающие ссылки в бургер-меню

--- wp-content/themes/ti-rodilsya/header.php
+++ wp-content/themes/ti-rodilsya/header.php
@@ -22,7 +22,7 @@
                 <div class="header__social">
                 </div>
                 <nav class="header__nav">
-                    <a href="<?php echo get_permalink(16); ?>" class="header__nav-item">Видеооткрытка с фильмами 1934 - 1994 год</a>
+                    <a href="<?php echo get_permalink(74); ?>" class="header__nav-item">Видеооткрытка с фильмами 1934 - 1994 год</a>
                     <a href="#" class="header__nav-item">Видеооткрытка с фильмами к праздникам</a>
                     <a href="#" class="header__nav-item">Индивидуальный проект</a>
                     <a href="#" class="header__nav-item">Оптовым покупателям</a>
@@ -35,8 +35,8 @@
                     <button id="closeBtn">
                         <i class="fa-solid fa-xmark"></i>
                     </button>
-                    <li><a href="#index.php?page=page-login">Личный кабинет (вход/регистрация)</a></li>
-                    <li><a href="#about">Видеооткрытка с фильмами 1934 - 1994 год</a></li>
+                    <li><a href="<?php echo get_permalink(12); ?>">Личный кабинет (вход/регистрация)</a></li>
+                    <li><a href="<?php echo get_permalink(74); ?>">Видеооткрытка с фильмами 1934 - 1994 год</a></li>
                     <li><a href="#services">Индивидуальный Проект</a></li>
                     <li><a href="#contact">Оптовым покупателям</a></li>
                     <li><a href="#contact">Корпоративным заказчикам</a></li>
@@ -45,6 +45,7 @@
                 <div class="header__login">
                 <?php if (is_user_logged_in()): ?>
                     <a class="header_login-link" href="<?php echo get_permalink(14); ?>">Профиль</a>
+                    <a class="header_login-link" href="<?php echo get_permalink(22); ?>">Корзина</a>
                     <a class="header_login-link" href="<?php echo wp_logout_url(home_url()); ?>">Выход</a>
                 <?php else: ?>
                     <a href="#" class="header_login-link">У меня есть код. Смотреть онлайн</a>
