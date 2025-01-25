0567df7fe6d023390c6fa1e67a21de000a751890
Vladislav Otdelyanov <vlad_1995@mail.ru>
2025-01-25 19:00:31 +0300

    Добавлено изображение.

--- wp-content/themes/ti-rodilsya/footer.php
+++ wp-content/themes/ti-rodilsya/footer.php
@@ -1,22 +1,53 @@
 <footer class="footer">
-        <div class="footer-item">
-            <a class="footer-item-link" href="#">- ФИЛЬМЫ ПО ГОДАМ</a>
-            <a class="footer-item-link" href="#">- ФИЛЬМЫ К ПРАЗДНИКАМ</a>
-            <a class="footer-item-link" href="#">- ИНДИВИДУАЛЬНОЕ ПРЕДЛОЖЕНИЕ</a>
+    <div class="footer-container">
+        <div class="footer-left">
+            <img class="footer-left-image" src="<?php echo get_template_directory_uri(); ?>/assets/img/Часы.png" alt="Часы">
         </div>
-        <div class="footer-item">
-            <a class="footer-item-link" href="#">- ОПТОВЫМ ПОКУПАТЕЛЯМ</a>
-            <a class="footer-item-link" href="#">- КОРПОРАТИВНЫМ ЗАКАЗЧИКАМ</a>
-            <a class="footer-item-link" href="#">- УНИВЕРМАН ПОДАРКОВ</a>
-        </div>
-        <div class="footer-copyright">
-            <div>
-
+        <div class="footer-center">
+            <h3 class="footer-center-title">Навигация</h3>
+            <div class="footer-center-content">
+                <div class="footer-center-nav">
+                    <nav class="nav-left">
+                        <a class="nav-link" href="#">Универмаг подарков</a>
+                        <a class="nav-link" href="#">Фильмы по годам</a>
+                        <a class="nav-link" href="#">Фильмы к праздникам</a>
+                    </nav>
+                    <nav class="nav-right">
+                        <a class="nav-link" href="#">Индивидуальный подарок</a>
+                        <a class="nav-link" href="#">Корпоративным заказчикам</a>
+                        <a class="nav-link" href="#">Оптовым покупателям</a>
+                    </nav>
+                </div>
+                <p class="footer-copyright">Торговая марка «Ты родился!» © Все права защищены</p>
             </div>
-
         </div>
-    </footer>
-    <?php wp_footer(); ?>
+        <div class="footer-right">
+            <h3 class="footer-right-title">Контакты</h3>
+            <div class="footer-right-content">
+                <div class="footer-right-contacts">
+                    <a class="contacts-phone" href="tel:89997776611">Телефон: 8 999 777 66 11</a>
+                    <a class="contacts-email" href="mailto:proekt.tr@gmail.com">Email: example@example.com</a>
+                </div>
+                <div class="footer-right-socials">
+                    <a class="footer-social-link" href="#">
+                        <i class="fa-brands fa-telegram"></i>
+                    </a>
+                    <a class="footer-social-link" href="#">
+                        <i class="fa-brands fa-whatsapp"></i>
+                    </a>
+                    <a class="footer-social-link" href="#">
+                        <i class="fa-brands fa-vk"></i>
+                    </a>
+                    <a class="footer-social-link" href="#">
+                        <i class="fa-brands fa-odnoklassniki"></i>
+                    </a>
+                </div>
+                <a href="#" class="footer-right-privacy">Политика конфиденциальности</a>
+            </div>
+        </div>
+    </div>
+</footer>
+<?php wp_footer(); ?>
 </body>
 
 </html>
