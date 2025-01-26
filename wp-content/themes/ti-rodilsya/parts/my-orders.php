7cddfe5eb7c33781bfd8806f45ea406276e8c307
Vladislav Otdelyanov <vlad_1995@mail.ru>
2025-01-26 17:37:54 +0300

    Добавлена страница "Мои заказы".

--- wp-content/themes/ti-rodilsya/parts/my-orders.php
+++ wp-content/themes/ti-rodilsya/parts/my-orders.php
@@ -4,4 +4,16 @@ Template Name: Шаблон раздела моих заказов
 */
 ?>
 
-<h2>Мои заказы</h2>
+<section class="orders-section">
+    <ul class="orders-list">
+        <li class="order-item">
+            <p class="order-date">09.09.2025</p>
+            <span>|</span>
+            <p class="order-number">Заказ №230</p>
+            <span>|</span>
+            <p class="order-status">Доставлен</p>
+        </li>
+    </ul>
+</section>
+
+
