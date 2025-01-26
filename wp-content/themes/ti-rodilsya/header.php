89e15b49e43a4fea3f7dc6596489f5d019edc59e
Влад Козлов <vkozlov15012005@gmail.com>
2025-01-26 16:28:08 +0300

    Исправлена работа ссылочной навигации

--- wp-content/themes/ti-rodilsya/header.php
+++ wp-content/themes/ti-rodilsya/header.php
@@ -22,7 +22,7 @@
                 <div class="header__social">
                 </div>
                 <nav class="header__nav">
-                    <a href="<?php echo get_permalink(74); ?>" class="header__nav-item">Видеооткрытка с фильмами 1934 - 1994 год</a>
+                    <a href="<?php echo get_permalink(122); ?>" class="header__nav-item">Видеооткрытка с фильмами 1934 - 1994 год</a>
                     <a href="#" class="header__nav-item">Видеооткрытка с фильмами к праздникам</a>
                     <a href="#" class="header__nav-item">Индивидуальный проект</a>
                     <a href="#" class="header__nav-item">Оптовым покупателям</a>
