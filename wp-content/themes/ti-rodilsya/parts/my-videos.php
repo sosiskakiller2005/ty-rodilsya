ae649f43e7ed3c18d09f83e3964775c2c38875fa
Vladislav Otdelyanov <vlad_1995@mail.ru>
2025-01-26 17:38:21 +0300

    Добавлена страница для раздела "Мои видеооткрытки".

--- wp-content/themes/ti-rodilsya/parts/my-videos.php
+++ wp-content/themes/ti-rodilsya/parts/my-videos.php
@@ -4,4 +4,32 @@ Template Name: Шаблон раздела моих виедооткрыток
 */
 ?>
 
-<h2>Мои видео</h2>
+<section class="videos-section">
+    <div class="input-code">
+        <h3 class="input-title">У меня есть код на просмотр DVD-открытки</h3>
+        <div class="input-block">
+            <input class="add-code-input" type="text" placeholder="Введите код">
+            <button class="add-code-button">Добавить</button>
+        </div>
+        <p class="input-code-message">
+            Если у вас есть DVD-открытка с кодом, введите его в данное поле
+        </p>
+    </div>
+    <div class="videos-list">
+        <h3 class="videos-list-title">Мои электронные открытки</h3>
+        <div class="videos">
+            <article class="video">
+                <img src="" alt="Видеообложка" class="video-cover">
+                <div class="video-content">
+                    <div class="content-main">
+                        <h3 class="content-title">Электронная открытка «1935 год»</h3>
+                        <span>
+                            <i class="fa-solid fa-circle-play"></i>
+                            <a class="video-link" href="#">Получить ссылку на открытку</a>
+                        </span>
+                    </div>
+                    <p class="last-added-video">Добавлено только что</p>
+                </div>
+            </article>
+        </div>
+</section>
