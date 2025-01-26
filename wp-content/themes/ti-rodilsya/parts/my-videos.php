429f29afd511d54f2495205af8adfcb1ac7e1351
Влад Козлов <vkozlov15012005@gmail.com>
2025-01-26 18:29:51 +0300

    Добавлена загрузка видеооткрыток, ссылка на ее просмотр

--- wp-content/themes/ti-rodilsya/parts/my-videos.php
+++ wp-content/themes/ti-rodilsya/parts/my-videos.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Template Name: Шаблон раздела моих виедооткрыток
+Template Name: Шаблон раздела моих видеооткрыток
 */
 ?>
 
@@ -18,18 +18,44 @@ Template Name: Шаблон раздела моих виедооткрыток
     <div class="videos-list">
         <h3 class="videos-list-title">Мои электронные открытки</h3>
         <div class="videos">
-            <article class="video">
-                <img src="" alt="Видеообложка" class="video-cover">
-                <div class="video-content">
-                    <div class="content-main">
-                        <h3 class="content-title">Электронная открытка «1935 год»</h3>
-                        <span>
-                            <i class="fa-solid fa-circle-play"></i>
-                            <a class="video-link" href="#">Получить ссылку на открытку</a>
-                        </span>
-                    </div>
-                    <p class="last-added-video">Добавлено только что</p>
-                </div>
-            </article>
+            <?php
+            $current_user = wp_get_current_user();
+            $customer_orders = wc_get_orders(array(
+                'customer_id' => $current_user->ID,
+                'status'      => 'completed',
+            ));
+
+            if (empty($customer_orders)) {
+                echo '<p>У вас пока нет покупок.</p>';
+            } else {
+                foreach ($customer_orders as $order) {
+                    foreach ($order->get_items() as $item_id => $item) {
+                        $product_id = $item->get_product_id();
+                        $product = wc_get_product($product_id);
+                        $video_link = get_post_meta($product_id, 'video_link', true);
+                        ?>
+                        <article class="video">
+                            <img src="<?php echo esc_url(wp_get_attachment_url($product->get_image_id())); ?>" alt="Видеообложка" class="video-cover">
+                            <div class="video-content">
+                                <div class="content-main">
+                                    <h3 class="content-title"><?php echo esc_html($product->get_name()); ?></h3>
+                                    <span>
+                                        <i class="fa-solid fa-circle-play"></i>
+                                        <?php if ($video_link): ?>
+                                            <a class="video-link" href="<?php echo esc_url($video_link); ?>" target="_blank">Получить ссылку на открытку</a>
+                                        <?php else: ?>
+                                            Видео не доступно.
+                                        <?php endif; ?>
+                                    </span>
+                                </div>
+                                <p class="last-added-video">Добавлено только что</p>
+                            </div>
+                        </article>
+                        <?php
+                    }
+                }
+            }
+            ?>
         </div>
+    </div>
 </section>
